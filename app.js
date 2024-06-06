c0926b9f56f460f28de2b1d6fd2c5a9f646ea892
Max-500 <213497@ids.upchiapas.edu.mx>
2024-06-06 10:35:06 -0600

    Pasando

--- app.js
+++ app.js
@@ -4,22 +4,6 @@ const app = express();
 // Middleware
 app.use(express.json());
 
-app.post('/api/v1/login', (req, res) => {
-  const {username, password} = req.body;
-  if (username === 'user' && password === 'pass') {
-    res.status(200).json({ message: 'user logged' });
-  } else {
-    res.status(401).json({ error: 'invalid credentials' });
-  }
-});
-
-app.get('/api/v1/users/:user', (req, res) => {
-  if (req.params.user === 'user') {
-    res.status(200).json({ message: 'user obtained' });
-  } else {
-    res.status(401).json({ error: 'user not found' });
-  }
-});
 
 app.get('/api/v1/welcome', (req, res) => {
   res.status(200).json({ message: 'Welcome to this Api ya 4' });
