e3371d00ecd6a528e48e916b9cdfb35e63b40ace
Max-500 <213497@ids.upchiapas.edu.mx>
2024-06-07 13:11:33 -0600

    Prueba

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/api/v1/welcome', (req, res) => {
   res.status(200).json({ message: 'Welcome Bitch!!!' });
 });
 
+app.get('/api/v1/help', (req, res) => {
+  res.status(200).json({ message: 'Helping Yadi!!!' });
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
